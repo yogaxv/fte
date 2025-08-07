cf6edac8b402350146dcf2b10eedf767a6c0fb72
yogaxv <yoga.xv@gmail.com>
2025-08-07 11:12:47 +0700

    first commit

---
+++ tests/Feature/Settings/ProfileUpdateTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\User;
+use Livewire\Volt\Volt;
+
+test('profile page is displayed', function () {
+    $this->actingAs($user = User::factory()->create());
+
+    $this->get('/settings/profile')->assertOk();
+});
+
+test('profile information can be updated', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user);
+
+    $response = Volt::test('settings.profile')
+        ->set('name', 'Test User')
+        ->call('updateProfileInformation');
+
+    $response->assertHasNoErrors();
+
+    $user->refresh();
+
+    expect($user->name)->toEqual('Test User');
+});
+
+test('user can delete their account', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user);
+
+    $response = Volt::test('settings.delete-user-form')
+        ->call('deleteUser');
+
+    $response->assertRedirect('/');
+
+    expect($user->fresh())->toBeNull();
+});
