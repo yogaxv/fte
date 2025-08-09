5c64b1f4cab6fcab62f736ee61e645c32cb3decc
yogaxv <yoga.xv@gmail.com>
2025-08-09 23:03:16 +0700

    logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center justify-center text-accent-foreground">
 {{--    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />--}}
-    <img src="./LOGO-eyeone.png" alt="EyeOne Logo" class="size-10" />
+    <img src="{{ asset('LOGO-eyeone.png') }}" alt="EyeOne Logo" class="size-8" />
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
     <span class="mb-0.5 truncate leading-tight font-semibold">EyeOne</span>
